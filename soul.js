5f1b133b3d649435e5d0c3e63c3dc727f7c9b3f6
rosegaipnazarova <rosegaipnazarova@gmail.com>
2026-01-07 11:11:24 +0500

    soul

--- soul.js
+++ soul.js
@@ -1,3 +1,46 @@
 //n1
 
+// function palindrom(str) {
+//     let str1 = str.toLowerCase()
+//     let str2 = str1.split('').reverse().join('')
+//     return str1 === str2
+// }
+// console.log(palindrom("apple"));
+
+//n2
+
+// function ikki(son) {
+//     return son.toString(2)
+// }
+// console.log(ikki(5));
+
+//n3
+
+// function swapElements(arr, i, j) {
+//     let narr = arr[i]
+//     arr[i] = arr[j]
+//     arr[j] = narr
+//     return arr
+// }
+// console.log(swapElements([1, 22, 13, 4], 1, 2)); 
+
+
+//n4
+
+// function letterFre(str) {
+//     let natija = {}
+//     for (let i of str) {
+//         if (i === " ") continue
+//         if (natija[i]) {
+//             natija[i]++
+//         } else {
+//             natija[i] = 1
+//         }
+//     }
+//     return natija
+// }
+// console.log(letterFre("java")); 
+
+
+//n5
 
